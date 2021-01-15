996ae01d2dfedfbcc02ca0b4a1ea3bd197ddc2c3
JoannaChandler <jhazimih@gmail.com>
2021-01-14 20:05:26 -0500

    Removing commented out code

--- js/app.js
+++ js/app.js
@@ -1,29 +1,3 @@
-// What is first?
-// Geolocation search box
-// Need a function grab/store user input value; and return the value
-    // input button
-    //Add click event to button to:
-        //update response URL with new location information
-        //update sidebar images with new images
-
-//SIDE BAR TASKS
-// "catSideBar", Petfinder API
-// "dogSideBar", Petfinder API
-// Make API Call (Ajax)
-// Log the response data
-// Get Pet Images based on GeoLocation
-// Display Pet Images in each sidebar
-    //Create and append img tags for each image
-    //Create and append any other tags for showing like age, breed, etc
-
-//RANDOM PHOTO TASKS
-// Random Pet Image, ????? API
-// Make API Call (Ajax)
-// Log response data
-// Display random image inside of #randomPhoto div
-// Add click event listener to button to run function to display and append the random image
-
-// Declare Variables
 var pfApiKey = 'hoxfe17CJiTbWmobl1EoByfn5PBmuon4tnV2xHc2sKXahYP3FG' 
 var pfSecretApiKey = '9MQ1EbHmO4Ew0mh9DbQduwBqONHPw610qvzwEWBd'
 
@@ -68,27 +42,7 @@ pf.animal.search(filter)
             $('#animalDiv' + [i]).append(animalImg, animalName, animalAge, contactInfo);
             $('#contactDiv' + [i]).append(phoneNumber, email);
         }
-        //Get the animal.photos array
-        //Loop through the array
-        //Create a div for each item
-        //Create an img tag for each item
-        //append img tag to the div for each item
-
-        /*Newvar.forEach(function(index){
-            var grid = document.querySelector('#photoGrid');
-            var div = document.createElement('div');
-            var img = document.createElement('img');
-            var info = document.createElement('div');
-            console.log(Newvar);
-            div.id = 'column' + index;
-            img.id = 'photo' + index;
-            img.setAttribute('src', '')//whatever the URL is
-
-            div.appendChild(img, info);
-            grid.appendChild(div);
-
-        }000)
-*/
+ 
         if(Newvar.length > 0) {
          //appendPhoto(Newvar[Math.floor(Math.random() * 10)].photos[0].large);
         } else {
